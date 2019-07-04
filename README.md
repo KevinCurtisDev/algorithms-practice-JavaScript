3d659c8be048ecfb0d737ec4bd47375a34c13ddd
kevin <kpcurtis@gmail.com>
2019-07-04 13:04:20 +0100

    Added capitalise challenge and my solution

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 A personal playground for solving algorithms using JavaScript. As a programmer you can never get enough practice with algorithms and data structures, so I set this simple little repo up in order to get a few challenges as regularly as I can manage. Feel free to clone the repo and test yourself with the challenges. 
 
-To run your code, make sure you have node installed. From your terminal cd into the file your challenge is located in, and run the command: node followed by the file name. 
+To run your code, make sure you have node installed. From your terminal cd into the folder your challenge file is located in, and run the command: node followed by the challenge file name. 
 
 ## example:
 You're working on the anagram.js file.
-* userName$ cd anagram.js
+* userName$ cd algorithms_JavaScript_practice
 * userName$ node anagram.js
 
---
+++ capitalise.js
@@ -0,0 +1,30 @@
+// Write a function that accepts a string.
+// The function should capitalise the first letter of each word in the string.
+// Return the capitalised string.
+
+
+//BOILERPLATE
+/*
+const capitalise = str => {
+
+    });
+    console.log(newStr);
+}
+*/
+
+const capitalise = str => {
+    strArray = str.split(" "); //split the string into an array
+    let newStr = strArray.map(word => {
+        //return the capitalised first letter of each word
+        //concatenated to each word with the first leter removed
+        return word[0].toUpperCase()+word
+                .slice(1, word.length);
+    });
+    console.log(newStr.join(" "));// log the joined array as the new string
+}
+
+
+//TESTS
+capitalise("hello, my name is kevin");
+capitalise("Have you ever wondered why?.. no? me neither");
+capitalise("1, 2, 3, 4, 5 once i caught a fish alive!");
